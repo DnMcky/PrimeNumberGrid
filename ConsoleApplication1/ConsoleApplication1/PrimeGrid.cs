33b98d86f41a4a5241bc13e745f58bc67bbabdee
DnMcky <danmackay@hotmail.co.uk>
2016-04-11 15:15:25 +0100

    Initial Commit
    C# project setup and test case to ensure that the user gets back the
    desired number of prime numbers in each axis of the prime multiplication
    grid.
    
    Test case too large, need to create a smaller test before writing code.

---
+++ ConsoleApplication1/ConsoleApplication1/PrimeGrid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class PrimeGrid
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public Array PrimeFinder(int numbeOfPrimesReq)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApplication1/PrimeGridUnitTest/PrimeGridTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace PrimeGridUnitTest
+{
+    [TestClass]
+    public class PrimeGridTests
+    {
+        [TestMethod]
+        public void TestPrimeFinder_ReturnsCorrectNoOfPrimes()
+        {
+            PrimeGrid target = new PrimeGrid();
+            int numbeOfPrimesReq = 5;
+            int expected = 5;
+            Array primeArray = target.PrimeFinder(numbeOfPrimesReq);
+            int actual = primeArray.Length;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
